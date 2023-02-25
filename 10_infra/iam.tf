4c5c318fdd0a7635eb242c9f417ec4e6649737c5
kmdryo <kmdryou010623@gmail.com>
2023-02-26 08:49:52 +0900

    slack修正4

--- 10_infra/iam.tf
+++ 10_infra/iam.tf
@@ -63,7 +63,6 @@ resource "aws_iam_role" "ecs_task_exec_iam_role" {
     Name    = "${var.project}-${var.environment}-ecs-task-exec-iam-role"
     Project = var.project
     Env     = var.environment
-    Message = "Hello World"
   }
 }
 
